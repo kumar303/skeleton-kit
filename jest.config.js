0c6df2e63a9ac17953809d4852c4398b3984886e
Kumar McMillan <kumar.mcmillan@gmail.com>
2020-06-23 23:24:19 -0500

    Optimized the tests a bit

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,11 @@ module.exports = {
   setupFiles: ["<rootDir>src/__tests__/setupJsDom.js"],
   setupFilesAfterEnv: ["<rootDir>src/__tests__/setupTests.tsx"],
   testEnvironment: "node",
-  testPathIgnorePatterns: ["/node_modules/", "<rootDir>/src/__tests__/setup.*"],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    "<rootDir>/build/",
+    "<rootDir>/src/__tests__/setup.*",
+  ],
   // This needs to match the url in __tests__/setupJsDom.js
   testURL: "http://localhost",
 };
--- src/__tests__/setupJsDom.js
+++ src/__tests__/setupJsDom.js
@@ -1,11 +1,5 @@
 const { JSDOM } = require("jsdom");
 
-const jsdom = new JSDOM("<!doctype html><html><body></body></html>", {
-  // This needs to match testURL in jest.config.js
-  url: "http://localhost",
-});
-const { window } = jsdom;
-
 function copyProps(src, target) {
   Object.defineProperties(target, {
     ...Object.getOwnPropertyDescriptors(src),
@@ -13,15 +7,24 @@ function copyProps(src, target) {
   });
 }
 
-global.window = window;
-global.document = window.document;
-global.navigator = {
-  userAgent: "node.js",
-};
-global.requestAnimationFrame = function (callback) {
-  return setTimeout(callback, 0);
-};
-global.cancelAnimationFrame = function (id) {
-  clearTimeout(id);
-};
-copyProps(window, global);
+if (!global.__alreadyAddedJsdomForTesting) {
+  const jsdom = new JSDOM("<!doctype html><html><body></body></html>", {
+    // This needs to match testURL in jest.config.js
+    url: "http://localhost",
+  });
+  const { window } = jsdom;
+
+  global.window = window;
+  global.document = window.document;
+  global.navigator = {
+    userAgent: "node.js",
+  };
+  global.requestAnimationFrame = function (callback) {
+    return setTimeout(callback, 0);
+  };
+  global.cancelAnimationFrame = function (id) {
+    clearTimeout(id);
+  };
+  copyProps(window, global);
+  global.__alreadyAddedJsdomForTesting = true;
+}
