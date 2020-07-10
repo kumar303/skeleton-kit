1685a998c6fecc04651c3332e3036cd1623406a2
Kumar McMillan <kumar.mcmillan@gmail.com>
2020-07-09 21:42:22 -0500

    Add some tests for initialContent

--- src/RealText.spec.tsx
+++ src/RealText.spec.tsx
@@ -1,18 +1,39 @@
 import React from "react";
-import { mount } from "enzyme";
+import { mount, shallow } from "enzyme";
 
 import { RealText } from ".";
 import InvisibleText from "./InvisibleText";
 import { Props as RealTextProps } from "./RealText";
+import { SkeletonTheme, getAppliedTheme } from "./theme";
+import MaybeSkeleton, { InitialContent } from "./utils/MaybeSkeleton";
 
 describe(__filename, () => {
-  function render({
+  type RenderProps = Partial<RealTextProps>;
+
+  function getRenderProps({
     children = "Example text",
     showSkeletons = true,
     ...moreProps
-  }: Partial<RealTextProps> = {}) {
-    const props = { children, showSkeletons, ...moreProps };
-    return mount(<RealText {...props} />);
+  }: RenderProps = {}) {
+    return { children, showSkeletons, ...moreProps };
+  }
+
+  function render(props: RenderProps = {}) {
+    return mount(<RealText {...getRenderProps(props)} />);
+  }
+
+  function renderInitialContent({
+    props = {},
+    theme = {},
+  }: { props?: RenderProps; theme?: Partial<SkeletonTheme> } = {}) {
+    const root = shallow(<RealText {...getRenderProps(props)} />);
+    const maybe = root.find(MaybeSkeleton);
+
+    expect(maybe).toHaveLength(1);
+    expect(maybe).toHaveProp("initialContent");
+
+    const initialContent = maybe.prop("initialContent") as InitialContent;
+    return initialContent(getAppliedTheme(theme));
   }
 
   it("renders invisible text when showing skeletons", () => {
@@ -28,5 +49,12 @@ describe(__filename, () => {
     const root = render({ className });
 
     expect(root.find("span").find(`.${className}`)).toHaveLength(1);
+  });
+
+  it("generates initial content using genSentence()", () => {
+    const initialCharCount = 25;
+    expect(renderInitialContent({ theme: { initialCharCount } })).toHaveLength(
+      initialCharCount
+    );
   });
 });
--- src/utils/MaybeSkeleton.tsx
+++ src/utils/MaybeSkeleton.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import { useTheme, SkeletonTheme } from "../theme";
 import { ChildrenType } from "./typeUtils";
 
+export type InitialContent = (theme: SkeletonTheme) => string;
+
 export interface Props {
   normalContent: ChildrenType;
   className: string | undefined;
   // TODO: make this required once I'm sure that it's a useful way to
   // configure initial content state.
-  initialContent?: (theme: SkeletonTheme) => string;
+  initialContent?: InitialContent;
   renderSkeleton: (content: ChildrenType) => JSX.Element;
 }
 
