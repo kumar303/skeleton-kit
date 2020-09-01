76cbf2dfad36f60278e8c8c4313b9ea919cfdf0e
Kumar McMillan <kumar.mcmillan@gmail.com>
2020-08-31 23:05:37 -0500

    Export prop types for all components

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
-export { default as BorderText } from "./BorderText";
-export { default as List } from "./List";
-export { default as Phrase } from "./Phrase";
-export { default as Shape } from "./Shape";
-export { default as SimulatedText } from "./SimulatedText";
-export { default as Skeleton } from "./Skeleton";
-export { default as SkeletonGroup } from "./SkeletonGroup";
-export { default as Text } from "./Text";
+export { default as BorderText, Props as BorderTextProps } from "./BorderText";
+export { default as List, Props as ListProps } from "./List";
+export { default as Phrase, Props as PhraseProps } from "./Phrase";
+export { default as Shape, Props as ShapeProps } from "./Shape";
+export {
+  default as SimulatedText,
+  Props as SimulatedTextProps,
+} from "./SimulatedText";
+export { default as Skeleton, Props as SkeletonProps } from "./Skeleton";
+export {
+  default as SkeletonGroup,
+  Props as SkeletonGroupProps,
+} from "./SkeletonGroup";
+export { default as Text, Props as TextProps } from "./Text";
