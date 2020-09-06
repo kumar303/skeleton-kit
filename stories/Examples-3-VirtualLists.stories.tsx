c6c416a8c7e868c7cbf101e7a64ff6ac2dead0dc
Kumar McMillan <kumar.mcmillan@gmail.com>
2020-09-06 16:49:59 -0500

    Got the basic website example working

---
+++ stories/Examples-3-VirtualLists.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta } from "@storybook/react/types-6-0";
+
+import VirtualLists from "./VirtualLists";
+
+const Example = VirtualLists;
+export { Example };
+
+export default {
+  title: "Examples/Virtual Lists",
+  component: VirtualLists,
+} as Meta;
---
+++ stories/VirtualLists/index.tsx
@@ -0,0 +1,132 @@
+import React, { PureComponent } from "react";
+
+import { FixedSizeList, ListChildComponentProps } from "react-window";
+import InfiniteLoader from "react-window-infinite-loader";
+
+import { Story } from "../helpers/styles";
+
+function ExampleWrapper({
+  // Are there more items to load?
+  // (This information comes from the most recent API request.)
+  hasNextPage,
+
+  // Are we currently loading a page of items?
+  // (This may be an in-flight flag in your Redux store for example.)
+  isNextPageLoading,
+
+  // Array of items loaded so far.
+  items,
+
+  // Callback function responsible for loading the next page of items.
+  loadNextPage,
+}: {
+  hasNextPage: boolean;
+  isNextPageLoading: boolean;
+  items: { name: string }[];
+  loadNextPage: (startIndex: number, stopIndex: number) => Promise<any> | null;
+}) {
+  // If there are more items to be loaded then add an extra row to hold a loading indicator.
+  const itemCount = hasNextPage ? items.length + 1 : items.length;
+
+  // Only load 1 page of items at a time.
+  // Pass an empty callback to InfiniteLoader in case it asks us to load more than once.
+  const loadMoreItems = isNextPageLoading ? () => null : loadNextPage;
+
+  // Every row is loaded except for our loading indicator row.
+  const isItemLoaded = (index: number) => !hasNextPage || index < items.length;
+
+  // Render an item or a loading indicator.
+  const Item = ({ index, style }: ListChildComponentProps) => {
+    let content;
+    if (!isItemLoaded(index)) {
+      content = "Loading...";
+    } else {
+      content = items[index].name;
+    }
+
+    return <div style={style}>{content}</div>;
+  };
+
+  return (
+    <InfiniteLoader
+      isItemLoaded={isItemLoaded}
+      itemCount={itemCount}
+      loadMoreItems={loadMoreItems}
+    >
+      {({ onItemsRendered, ref }) => (
+        <FixedSizeList
+          itemCount={itemCount}
+          onItemsRendered={onItemsRendered}
+          ref={ref}
+          height={150}
+          itemSize={35}
+          width={300}
+        >
+          {Item}
+        </FixedSizeList>
+      )}
+    </InfiniteLoader>
+  );
+}
+
+type Props = Record<string, unknown>;
+
+type State = {
+  hasNextPage: boolean;
+  isNextPageLoading: boolean;
+  items: { name: string }[];
+};
+
+class App extends PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      hasNextPage: true,
+      isNextPageLoading: false,
+      items: [],
+    };
+  }
+
+  _loadNextPage = () => {
+    return new Promise((resolve) => {
+      this.setState({ isNextPageLoading: true }, () => {
+        setTimeout(() => {
+          this.setState((state) => ({
+            hasNextPage: state.items.length < 100,
+            isNextPageLoading: false,
+            items: [...state.items].concat(
+              new Array(10).fill(true).map(() => ({ name: "Example name" }))
+            ),
+          }));
+          resolve(null);
+        }, 2500);
+      });
+    });
+  };
+
+  render() {
+    const { hasNextPage, isNextPageLoading, items } = this.state;
+
+    return (
+      <ExampleWrapper
+        hasNextPage={hasNextPage}
+        isNextPageLoading={isNextPageLoading}
+        items={items}
+        loadNextPage={this._loadNextPage}
+      />
+    );
+  }
+}
+
+const VirtualListsStory: React.FunctionComponent<Record<
+  string,
+  unknown
+>> = () => {
+  return (
+    <Story>
+      <App />
+    </Story>
+  );
+};
+
+export default VirtualListsStory;
