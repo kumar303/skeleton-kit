e73c20081fe34adb4d9cacaa50cfcbb83c8eb883
Kumar McMillan <kumar.mcmillan@gmail.com>
2020-09-07 21:38:24 -0500

    Do not generate random classes constantly

--- src/OpacityPulse.tsx
+++ src/OpacityPulse.tsx
@@ -1,9 +1,7 @@
 import styled, { keyframes } from "styled-components";
 
 // TODO: make opacity configurable.
-// This requires return keyframes from a function so that
-// it can access the theme through a function argument.
-// https://github.com/styled-components/styled-components/issues/397
+// See https://github.com/kumar303/skeleton-kit/issues/13
 const pulseOpacity = keyframes`
   0% {
     opacity: 0.2;
@@ -26,21 +24,25 @@ const pulseOpacity = keyframes`
   }
 `;
 
-const OpacityPulse = styled.span`
+const OpacityPulse = styled.span.attrs((props) => {
+  const max = props.theme.skeletonKit.loopDurationMaxSec;
+  const min = props.theme.skeletonKit.loopDurationMinSec;
+  const range = max - min;
+  if (range < 0) {
+    throw new Error(
+      `loopDurationMinSec (${min}) cannot be greater than loopDurationMax (${max})`
+    );
+  }
+
+  const animationDuration = `${Math.random() * range + min}s`;
+
+  return {
+    style: {
+      animationDuration,
+    },
+  };
+})`
   animation: ${pulseOpacity} infinite cubic-bezier(0.65, 0.05, 0.36, 1);
-  animation-duration: ${(props) => {
-    const max = props.theme.skeletonKit.loopDurationMaxSec;
-    const min = props.theme.skeletonKit.loopDurationMinSec;
-    const range = max - min;
-    if (range < 0) {
-      throw new Error(
-        `loopDurationMinSec (${min}) cannot be greater than loopDurationMax (${max})`
-      );
-    }
-
-    return `${Math.random() * range + min}s`;
-  }};
-
   @media (prefers-reduced-motion: reduce) {
     animation: none;
   }
