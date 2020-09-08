74b19c98788029dbf63b28746f8a42f763cf0e6a
Kumar McMillan <kumar.mcmillan@gmail.com>
2020-09-07 21:38:46 -0500

    Tighten up virtual lists example

--- stories/VirtualLists/index.tsx
+++ stories/VirtualLists/index.tsx
@@ -3,9 +3,9 @@ import React, { useState } from "react";
 import { FixedSizeList, ListChildComponentProps } from "react-window";
 import InfiniteLoader from "react-window-infinite-loader";
 
+import { Phrase, SkeletonGroup } from "../../src";
 import { names } from "../helpers/butterflyData";
 import { getLoadSpeed } from "../helpers/knobs";
-import { Phrase, SkeletonGroup } from "../../src";
 import {
   CtrlButton,
   Content,
@@ -30,45 +30,31 @@ const VirtualListsStory: React.FunctionComponent<Record<
   const loadSpeed = getLoadSpeed();
 
   const [loaderKey, setLoaderKey] = useState<string>(genLoaderKey());
-
-  // Are there more items to load?
-  // (This information comes from the most recent API request.)
-  const [hasNextPage, setHasNextPage] = useState<boolean>(true);
-
-  // Are we currently loading a page of items?
-  // (This may be an in-flight flag in your Redux store for example.)
   const [isNextPageLoading, setIsNextPageLoading] = useState<boolean>(false);
-
-  // Array of items loaded so far.
   const [items, setItems] = useState<Item[]>([]);
 
-  // If there are more items to be loaded then add extra rows to hold the skeletons.
-  const itemCount = hasNextPage ? items.length + 10 : items.length;
+  // Add extra rows to hold the skeletons.
+  const itemCount = items.length + 10;
 
-  // Every row is loaded except for our loading indicator row.
-  const isItemLoaded = (index: number) => !hasNextPage || index < items.length;
+  const isItemLoaded = (index: number) => index < items.length;
 
-  // Callback function responsible for loading the next page of items.
   const loadNextPage = () => {
+    if (isNextPageLoading) {
+      return null;
+    }
     setIsNextPageLoading(true);
 
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        setHasNextPage(items.length < 100);
-        setIsNextPageLoading(false);
-        setItems([...items].concat(names.map((n) => ({ name: n }))));
-        resolve(null);
-      }, loadSpeed * 1000);
-    });
+    // Simulate a slow API response.
+    setTimeout(() => {
+      setIsNextPageLoading(false);
+      setItems([...items].concat(names.map((n) => ({ name: n }))));
+    }, loadSpeed * 1000);
+
+    return null;
   };
 
-  // Only load 1 page of items at a time.
-  // Pass an empty callback to InfiniteLoader in case it asks us to load more than once.
-  const loadMoreItems = isNextPageLoading ? () => null : loadNextPage;
-
   const Item = ({ index, style }: ListChildComponentProps) => {
-    const showSkeletons = !isItemLoaded(index);
-    const content = showSkeletons ? "" : items[index].name;
+    const content = items[index] && items[index].name;
 
     return (
       <StyledItem altBg={Boolean(index % 2)} style={style}>
@@ -99,15 +85,18 @@ const VirtualListsStory: React.FunctionComponent<Record<
               key={loaderKey}
               isItemLoaded={isItemLoaded}
               itemCount={itemCount}
-              loadMoreItems={loadMoreItems}
+              loadMoreItems={loadNextPage}
             >
               {({ onItemsRendered, ref }) => (
                 <FixedSizeList
                   itemCount={itemCount}
+                  layout="vertical"
                   onItemsRendered={onItemsRendered}
                   ref={ref}
                   height={contentWidth}
-                  itemSize={35}
+                  // This is the height in pixels of each item.
+                  // This assumes 1rem = 16px.
+                  itemSize={3 * 16}
                   width={contentWidth}
                 >
                   {Item}
